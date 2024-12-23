94670453226450ff7f440c8acfcf25dbf02f94a5
自分のユーザ名 <>
2024-12-23 14:27:22 +0900

    adjust odrive

--- ODRIVE_only/202411/calib.py
+++ ODRIVE_only/202411/calib.py
@@ -11,10 +11,8 @@ AXIS_STATE_CLOSED_LOOP_CONTROL = 8
 VELOCITY_CONTROL = 2
 
 # Find two ODrives
-# maxon
 odrv0 = odrive.find_any(serial_number='385B34743539')
-#R100
-# odrv1 = odrive.find_any(serial_number='385E344A3539')
+odrv1 = odrive.find_any(serial_number='385E344A3539')
 
 odrv0.axis0.requested_state = MOTOR_CALIBRATION
-# odrv1.axis0.requested_state = MOTOR_CALIBRATION
+odrv1.axis0.requested_state = MOTOR_CALIBRATION
