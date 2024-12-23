06dffb01a9f5c78df4ccbedd2af1c534fbf18270
自分のユーザ名 <>
2024-12-23 16:21:27 +0900

    impulse input

--- ODRIVE_only/202412/trap.py
+++ ODRIVE_only/202412/trap.py
@@ -41,12 +41,12 @@ initial_position0 = odrv0.axis0.pos_vel_mapper.pos_rel
 initial_position1 = odrv1.axis0.pos_vel_mapper.pos_rel
 # initial_position2 = odrv2.axis0.pos_vel_mapper.pos_rel
 
-odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
 odrv0.axis0.controller.config.control_mode = ControlMode.TORQUE_CONTROL
 odrv0.axis0.config.motor.torque_constant = 0.106 #(トルク定数 8.23/Kv)
 odrv0.axis0.controller.input_torque = 0
 
-odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv1.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
 odrv1.axis0.controller.config.control_mode = ControlMode.TORQUE_CONTROL
 odrv1.axis0.config.motor.torque_constant = 0.106 #(トルク定数 8.23/Kv)
 odrv1.axis0.controller.input_torque = 0
@@ -62,12 +62,12 @@ time1 = 0
 # Kd1 = 10 # Derivative gain
 
 ## motor only
-Kp0 = 50 # Proportional gain
-Ki0 = 8  # Integral gain
-Kd0 = 2 # Derivative gain
-Kp1 = 50  # Proportional gain
-Ki1 = 8 # Integral gain
-Kd1 = 2 # Derivative gain
+Kp0 = 2.8 # Proportional gain
+Ki0 = 0.8  # Integral gain
+Kd0 = 0.15 # Derivative gain
+Kp1 = 2.5  # Proportional gain
+Ki1 = 0.65 # Integral gain
+Kd1 = 0.15 # Derivative gain
 prev_error0 = 0
 prev_error1 = 0
 prev_time = time.time()
@@ -88,6 +88,11 @@ ref1 = []
 error_queue0 = deque(maxlen=5)
 error_queue1 = deque(maxlen=5)
 
+# Define impulse parameters
+impulse_time = 1.0  # Time after which to apply the impulse
+impulse_duration = 0.005  # Duration of the impulse in seconds
+impulse_position = 0.01  # Position to set during the impulse
+
 try:
     while True:
         # Calculate the elapsed time
@@ -99,75 +104,6 @@ try:
         current_pos0 = odrv0.axis0.pos_vel_mapper.pos_rel-initial_position0
         current_pos1 = odrv1.axis0.pos_vel_mapper.pos_rel-initial_position1
 
-        if elapsed_time > 0:
-            if elapsed_time <= 1:
-                desired_pos0 = 0
-                desired_pos1 = 0
-            else:
-                desired_pos0 =  -0.1 * np.sin(5 * elapsed_time)
-                desired_pos1 =  -0.1 * np.sin(5 * elapsed_time)
-
-            # 指令値をリストに格納
-            ref0.append(desired_pos0)
-            ref1.append(desired_pos1)
-             
-            # Calculate the error - initial_position0
-            # 誤差の計算
-            error0 = desired_pos0 - current_pos0
-            error1 = desired_pos1 - current_pos1
-            # 誤差をデックに追加
-            error_queue0.append(error0)
-            error_queue1.append(error1)
-            # error_integral0 = max(min(error_integral0, integral_limit), -integral_limit)
-            # error_integral1 = max(min(error_integral1, integral_limit), -integral_limit)
-            error_integral0 += error0 * time_diff
-            error_integral1 += error1 * time_diff
-
-            # 誤差の微分項の計算
-            if len(error_queue0) == 5:
-                error_derivative0 = (error_queue0[-1] - error_queue0[0]) / (5 * time_diff)
-            else:
-                error_derivative0 = 0
-
-            if len(error_queue1) == 5:
-                error_derivative1 = (error_queue1[-1] - error_queue1[0]) / (5 * time_diff)
-            else:
-                error_derivative1 = 0
-            
-            # Calculate the new torque input
-            new_torque0 = Kp0 * error0 + Kd0 * error_derivative0 + Ki0 * error_integral0
-            new_torque1 = Kp1 * error1 + Kd1 * error_derivative1 + Ki1 * error_integral1
-
-            max_float32 = 3.4e38
-            if new_torque0 > max_float32:
-                new_torque0 = max_float32
-            elif new_torque0 < -max_float32:    
-                new_torque0 = -max_float32
-            if new_torque1 > max_float32:
-                new_torque1 = max_float32
-            elif new_torque1 < -max_float32:
-                new_torque1 = -max_float32
-            
-            # Set the new torque input
-            odrv0.axis0.controller.input_torque = new_torque0
-            odrv1.axis0.controller.input_torque = new_torque1
-
-            # Update the previous error
-            prev_error0 = error0
-            prev_error1 = error1
-
-            input_torque0.append(odrv0.axis0.controller.input_torque)
-            input_torque1.append(odrv1.axis0.controller.input_torque)
-        else:
-            desired_pos0 = 0
-            desired_pos1 = 0
-            filtered_desired_pos0 = 0
-            filtered_desired_pos1 = 0
-            input_torque0.append(odrv0.axis0.controller.input_torque)
-            input_torque1.append(odrv1.axis0.controller.input_torque)
-            ref0.append(filtered_desired_pos0)
-            ref1.append(filtered_desired_pos1)
-
         # Add the data to the list
         current_data_0.append( math.sqrt(odrv0.axis0.motor.foc.Iq_measured**2 + odrv0.axis0.motor.foc.Id_measured**2))
         vel_data_0.append(360*math.pi*odrv0.axis0.pos_vel_mapper.vel/180)
@@ -176,6 +112,18 @@ try:
         vel_data_1.append(360*math.pi*odrv1.axis0.pos_vel_mapper.vel/180)
         position_data_1.append(current_pos1)
         time_data.append(elapsed_time)
+
+        # Apply impulse position input at the specified time
+        if not loop_executed and impulse_time <= elapsed_time < impulse_time + impulse_duration:
+            odrv0.axis0.controller.input_pos += impulse_position
+            odrv1.axis0.controller.input_pos += impulse_position
+            loop_executed = True
+        else:
+            # Set the position input back to the desired position after the impulse
+            desired_pos0 = -0.1 * math.sin(5 * elapsed_time)
+            desired_pos1 = -0.1 * math.sin(5 * elapsed_time)
+            odrv0.axis0.controller.input_pos = initial_position0
+            odrv1.axis0.controller.input_pos = initial_position1
                 
 except KeyboardInterrupt:    
     now = datetime.now()
